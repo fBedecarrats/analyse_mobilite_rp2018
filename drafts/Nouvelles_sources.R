35456e9114883891c4e94323767b4da2b8776358
FlorentBedecarratsNM <florent.bedecarrats@nantesmetropole.fr>
2022-09-13 22:39:24 +0200

    Remove unnecessary files

--- drafts/Nouvelles_sources.R
+++
@@ -1,8 +0,0 @@
-# 2018 : https://www.insee.fr/fr/statistiques/fichier/5393835/base-csv-flux-mobilite-domicile-lieu-travail-2018.zip
-# 2017 : https://www.insee.fr/fr/statistiques/fichier/4509353/base-csv-flux-mobilite-domicile-lieu-travail-2017.zip
-# 2016 txt : https://www.insee.fr/fr/statistiques/fichier/4171554/base-texte-flux-mobilite-domicile-lieu-travail-2016.zip
-# 2015 txt : https://www.insee.fr/fr/statistiques/fichier/3566477/base-texte-flux-mobilite-domicile-lieu-travail-2015.zip
-# 2014 : https://www.insee.fr/fr/statistiques/fichier/2862065/base-texte-flux-mobilite-domicile-lieu-travail-2014.zip
-# 2013 : https://www.insee.fr/fr/statistiques/fichier/2022109/base-texte-flux-mobilite-domicile-lieu-travail-2013.zip
-
-
